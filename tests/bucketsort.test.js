6281b172a7fee4f8bd833b316bc727aad1fb1e64
lucasvavon <lucasvavon@icloud.com>
2023-02-07 16:19:06 +0100

    commit 5

--- tests/bucketsort.test.js
+++ tests/bucketsort.test.js
@@ -5,6 +5,7 @@ let input1 = [];
 let input2 = [1.9];
 let input3 = [18.12, 1.9];
 let input4 = [1.7, 1.9]
+let input5 = [1.9, 1.7, 4.3, 2.3, 2.2, 2.1, 8.1]
 
 test('return bucket concat without value', function () {
     expect(bucketConcat(input1)).toStrictEqual([]);
@@ -20,6 +21,10 @@ test('return bucket concat with two value', function () {
 
 test('return bucket concat with two value already sorted', function () {
     expect(bucketConcat(input4)).toStrictEqual([1.7, 1.9]);
+});
+
+test('return bucket concat with two value already sorted', function () {
+    expect(bucketConcat(input5)).toStrictEqual([1.7, 1.9, 2.1, 2.2, 2.3, 4.3, 8.1]);
 });
 
 
